b5d7a5859c4a799c54779af72817d6c1819ca533
suresh <sureshhallyali@gmail.com>
2020-11-04 00:50:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Hello Suresh H
         </a>
       </header>
     </div>
